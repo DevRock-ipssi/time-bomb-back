7161824e4c21f96325bb8370afafefb58819e0c1
anta <antandoye0@gmail.com>
2020-07-28 00:16:05 +0200

    Update

--- src/app.js
+++ src/app.js
@@ -5,26 +5,7 @@ const hostname = '0.0.0.0';
 const port = 3000;
 
 const mongoose = require('mongoose');
-<<<<<<< HEAD
 mongoose.connect('mongodb://mongo/timeBomb');
-=======
-
-const db = async () => {
-	try {
-		await mongoose.connect('mongodb://mongo/time_bomb', {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-			useCreateIndex: true,
-			useFindAndModify: false // To Suppress deprecated warning messages
-		});
-		console.log('DB connected');
-	} catch (error) {
-		console.log(`Connection Error : ${error}`);
-	}
-};
-// Execute DB connection
-db();   
->>>>>>> cab2e6567631577e9d20a4bace7eb155ae5b6f98
 
 const bodyParser = require('body-parser');
 server.use(bodyParser.urlencoded({ extended: true }));
@@ -36,7 +17,6 @@ roleRoute(server);
 const cors = require('cors');
 server.use(cors());
 
-<<<<<<< HEAD
 
 /*server.get('/', (req, res )=> {
     res.send('hello')
@@ -49,8 +29,6 @@ postRoute(server);
 const commentRoute = require('./api/routes/commentRoute');
 commentRoute(server);
 */
-=======
->>>>>>> cab2e6567631577e9d20a4bace7eb155ae5b6f98
 const userRoute = require('./api/routes/userRoute');
 userRoute(server);
 
